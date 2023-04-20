b62995ad52332f08cf181aaf442d4059a5c82fed
wrathchild14 <jovanprodanov123@gmail.com>
2023-04-20 13:41:41 +0200

    Add more utility functions

--- Sources/Utility/Common.h
+++ Sources/Utility/Common.h
@@ -1,19 +1,36 @@
+#pragma once
 #include <cmath>
 #include <limits>
-#include <memory>
 
 // Constants
-
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
-
-inline double DegreesToRadians(const double degrees) {
+inline double DegreesToRadians(const double degrees)
+{
 	return degrees * pi / 180.0;
 }
 
-// Common Headers
+inline double RandomDouble()
+{
+	// Returns a random real in [0,1).
+	return rand() / (RAND_MAX + 1.0);
+}
 
+inline double RandomDouble(double min, double max)
+{
+	// Returns a random real in [min,max).
+	return min + (max - min) * RandomDouble();
+}
+
+inline double Clamp(const double x, const double min, const double max)
+{
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
+}
+
+// Common Headers
 #include "Ray.h"
 #include "Vec3.h"
